ba0faefd388c1e9e59d10e2f7313ef21d458b0a7
Rougui224 <rouguiatoubarry559@gmail.com>
2026-02-16 17:33:14 +0100

    AFRI-20 update location map

--- src/components/location/location.tsx
+++ src/components/location/location.tsx
@@ -7,7 +7,7 @@ export default function Location() {
 
             <div className="w-full h-96">
                 <iframe
-                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3858.9223572288556!2d-17.478427709193493!3d14.721369893117503!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f16!3m3!1m2!1s0xec172aeadb74d39%3A0x230af86d2cf852bb!2sSacre%20Coeur%203%20Extension%2C%20Dakar%2C%20Senegal!5e0!3m2!1sen!2sfr!4v1763583538146!5m2!1sen!2sfr"
+                    src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d3857.7105111707515!2d-17.365283924890264!3d14.78532898572319!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zMTTCsDQ3JzA3LjIiTiAxN8KwMjEnNDUuOCJX!5e0!3m2!1sen!2sfr!4v1771258971065!5m2!1sen!2sfr"
                     width="100%"
                     height="450"
                     title="La localisation de afriAi Solutions"
